a578756fde8464ad498af9ed255b8715b5c4ce7d
Joe O'Connor <joeoc2625@gmail.com>
2021-08-28 13:38:05 +0100

    Clean up on run

--- runner/run.sh
+++ runner/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker container prune -f
+docker volume prune -f
 docker pull aiwarssoc/sandbox
 python -m gunicorn --workers=1 --threads=3 --worker-class=uvicorn.workers.UvicornWorker --worker-connections=1000 --bind 0.0.0.0:8080 --log-level debug main:app --timeout 30
